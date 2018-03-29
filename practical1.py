0ef571b0755a578f39e8d7f080b7bd2dec84f93e
Deep Parmar <deep56parmar@hotmail.com>
2018-03-29 17:54:26 +0530

    with armstrong

--- practical1.py
+++ practical1.py
@@ -2,4 +2,26 @@
 #print "Hello"
 print ("Hello")
 
-#
+#greater in 3 integers
+a,b,c = 50,70,30
+print(a if (a>b and a>c) else (b if (b>a and b>c) else c))
+
+#Armstring number checker
+number = 371
+num = number
+sum = 0
+length = len(str(number))
+count = length
+while  count:
+    x = number % 10
+    sum += x ** length
+    number = number // 10
+    count = count -1
+if(sum == num):
+    print("number is armstrong")
+
+#prime number
+
+
+#palindrome
+#Fibonacci
