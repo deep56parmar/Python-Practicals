39ddb30e9726babdbc1ffd99d1c864f42725ae33
Deep Parmar <deep56parmar@hotmail.com>
2018-03-29 19:26:03 +0530

    Practical 2 and 3 added

---
+++ practical2.py
@@ -0,0 +1,10 @@
+string = "Hi I am Deep Parmar."
+string2 = ""
+string3 = ""
+print(len(string)) # length of string
+string2 += string
+print (string2) #string copy
+string3 = string + " I am an Engineer."
+print(string3) # string concatination.
+string3 =  string3.replace("Engineer.","Scientist.")
+print(string3)
---
+++ practical3.py
@@ -0,0 +1,11 @@
+def swap(x,y):
+    x,y = y,x
+    print ("value of x is " , x ," value of y is " , y)
+def stringLength(str):
+    count = 0
+    for i in str:
+        count += 1
+    return count
+
+swap (10,20)
+print(stringLength("Hi My name is deep"))
